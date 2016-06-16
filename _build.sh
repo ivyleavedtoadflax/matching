dbb97f47d78da6aa8b0d3500879084574d74d4ab
Matthew Upson <ivyleavedtoadflax@gmail.com>
2016-06-16 08:55:51 +0100

    Corrected arguments

--- _build.sh
+++ _build.sh
@@ -2,7 +2,7 @@
 
 rmarkdown::render(
     input = "index.Rmd", 
-    out_format = "html", 
-    out_file = "index.html"
+    output_format = "html_document", 
+    output_file = "index.html"
 )
 #bookdown::render_book("index.Rmd", "bookdown::pdf_book")
